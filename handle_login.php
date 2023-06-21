bd2e19acfa492c6377b17e5a0d5758dbbd81e41c
Aldrin Caballero <caballeroaldrin02@gmail.com>
2023-06-22 07:19:18 +0800

    modified:   handle_login.php

--- handle_login.php
+++ handle_login.php
@@ -8,6 +8,17 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $post['email'];
     $password = $post['password'];
 
-    
+    // check if admin
+    if(isDataExists("user_accounts", "*", "email = '$email' AND password = '$password'")) {
+        
+    } else {
+        // no records found
+        // Invalid email or password
+        echo '
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                Invalid email or password! please try again.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>';
+    }
 
 }
--- handle_register.php
+++ handle_register.php
@@ -18,17 +18,13 @@ function validateFullName($fullName) {
 }
 
 
-
-
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $post = validate_post_data($_POST);
-
-    $fullname = $post['fullname'];
-    $uid = uniqid();
+    $fullname = ucwords(strtolower($post['fullname']));
     $email = $post['email'];
     $password = $post['password'];
     $contact_no = $post['contact_no'];
-    $address = $post['address'];
+    $address = ucwords(strtolower($post['address']));
     $birthday = $post['birthday'];
     $gender = $post['gender'];
     $age = $post['age'];
@@ -41,7 +37,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
                 Fullname is not valid!
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>';
-    } elseif(isDataExists("account_registration", "email", "email = '$email'")) {
+    } elseif(isDataExists("user_accounts", "email", "email = '$email'")) {
         echo '
             <div class="alert alert-warning alert-dismissible fade show" role="alert">
                 Email already exist!
@@ -56,10 +52,12 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
         ';
     } else {
         // insert data
-        $sql = "INSERT INTO account_registration(fullname, unique_id, email, password, contact_no, address, birthday, gender, age, status) VALUES('$fullname','$uid','$email','$password','$contact_no','$address','$birthday','$gender','$age','$status')";
+        $sql = "INSERT INTO user_accounts(fullname, user_type, email, password, contact_no, address, birthday, gender, age, status) VALUES('$fullname','client','$email','$password','$contact_no','$address','$birthday','$gender','$age','$status')";
         if(mysqli_query($conn, $sql)) {
             $_SESSION['login'] = true;
-            $_SESSION['unique_id'] = $uid;
+            $_SESSION['user_type'] = 'client';
+            $_SESSION['fullname'] = $fullname;
+            $_SESSION['email'] = $email;
             echo '';
         } else {
              echo '
