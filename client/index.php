cbf3ff3cbef755335dddc610027f61f2f417a996
Aldrin Caballero <caballeroaldrin02@gmail.com>
2023-06-29 11:04:57 +0800

    new file:   src/assets/wedding.png

--- client/index.php
+++ client/index.php
@@ -19,39 +19,23 @@ if(isset($_SESSION['login']) && $_SESSION['login']) {
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Client | Municipality of Rizal</title>
+    <title>Admin | Municipality of Rizal</title>
     <link rel="icon" href="../src/assets/logo.png" type="image/x-icon" />
-
-    <!-- Jquery -->
     <script src="../src/jquery/jquery.min.js"></script>
-
-    <!-- sweet alert -->
     <script src="../src/sweetalert2/sweetalert2.all.min.js"></script>
-
-    <!-- axios -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.4.0/axios.min.js"
         integrity="sha512-uMtXmF28A2Ab/JJO2t/vYhlaa/3ahUOgj1Zf27M5rOo8/+fcTUVH0/E0ll68njmjrLqOBjXM3V9NiPFL5ywWPQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-    <!-- w3JS -->
     <script src="https://www.w3schools.com/lib/w3.js"></script>
-
-    <!-- fotn-awesome -->
     <link rel="stylesheet" href="../src/fontawesome/css/all.min.css" />
-
-    <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
     </script>
-
-    <!-- Poppins font -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
-
-    <!-- google icons -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 
@@ -62,7 +46,6 @@ if(isset($_SESSION['login']) && $_SESSION['login']) {
         padding: 0;
         margin: 0;
         box-sizing: border-box;
-        transition: all 0.5s;
     }
 
     body::-webkit-scrollbar {
@@ -110,48 +93,55 @@ if(isset($_SESSION['login']) && $_SESSION['login']) {
             <h6 class="mt-2">
                 <?php echo $_SESSION['fullname'] ?>
             </h6>
-            <img src="../src/assets/profile-avatar.png" alt="" height="50px" class="rounded-circle">
+
+            <div class="dropdown">
+                <img src="../src/assets/profile-avatar.png" alt="" height="50px" style="cursor: pointer;"
+                    class="rounded-circle hover" data-bs-toggle="dropdown" aria-expanded="false">
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item hover d-flex align-items-center justify-content-start gap-2"
+                            href="#"><span class="material-symbols-outlined">person</span> Profile</a>
+                    </li>
+                    <li><a class="dropdown-item hover d-flex align-items-center justify-content-start gap-2"
+                            href="../logout.php"><span class="material-symbols-outlined">logout</span> Logout</a></li>
+                </ul>
+            </div>
         </div>
     </div>
 
     <div style="
         min-height: 80vh;
-      " class="container-fluid row p-0">
-        <div class="col-5 col-md-3 p-0">
-            <img src="../src/assets/Munisipyo_Facade.jpg" class="img-fluid">
-            <a class="nav-link active bg-secondary text-white d-flex align-items-center justify-content-start gap-2 border fs-5 p-3" href="./">
-                <span class="material-symbols-outlined">home</span> Dashboard</a>
-            <a class="nav-link d-flex align-items-center justify-content-start gap-2 border fs-5 p-3" href="./">
-                <span class="material-symbols-outlined">badge</span> Voters certificate</a>
-            <a class="nav-link d-flex align-items-center justify-content-start gap-2 border fs-5 p-3" href="./">
-                <span class="material-symbols-outlined">demography</span> PSA</a>
-            <a class="nav-link d-flex align-items-center justify-content-start gap-2 border fs-5 p-3" href="./">
-                <span class="material-symbols-outlined">camera_rear</span> Burial Assistance</a>
-            <a class="nav-link d-flex align-items-center justify-content-start gap-2 border fs-5 p-3" href="./">
-                <span class="material-symbols-outlined">diversity_3</span> kasalang Bayan</a>
-            <a class="nav-link d-flex align-items-center justify-content-start gap-2 border fs-5 p-3" href="./">
-                <span class="material-symbols-outlined">insights</span> Data Analytics</a>
+      " class="container-fluid p-4 text-center">
+        <button class="btn btn-lg px-5 py-3 border-0 fw-bold rounded-0 col-4"
+            style="background-image: linear-gradient(white, skyblue);">E-Services</button>
+
+
+        <div class="row justify-content-center mt-4 gap-3">
+            <a href="#" style="box-shadow: 1px 0px 5px 9px rgba(255,255,255,0.6) inset;"
+                class="col border-0 btn col-md-3 p-4 text-center text-white bg-success">
+                <img src="https://www.mnchamber.com/sites/default/files/inline-images/Voting%20icon%20square.jpg"
+                    class="rounded-circle border" style="height: 150px; width: 150px;" alt="voters certificate"><br><br>
+                <h4>Voters certificate</h4>
+            </a>
+            <a href="#" style="box-shadow: 1px 0px 5px 9px rgba(255,255,255,0.6) inset;"
+                class="col border-0 btn col-md-3 p-4 text-center text-white bg-success ">
+                <img src="https://rsso05.psa.gov.ph/sites/default/files/Philippine_Statistics_Authority.png"
+                    class="rounded-circle border" style="height: 150px; width: 150px;" alt="voters certificate"><br><br>
+                <h4>PSA</h4>
+            </a>
         </div>
-        <div class="col-7 col-md-9 text-center p-md-4 p-2">
-            <div>
-                <button class="btn btn-success px-5 fs-4">Dashboard</button>
-            </div>
-            <div class="row justify-content-center gap-5 mt-5">
-                <div class="col col-md-4 bg-success text-white p-5 shadow rounded d-flex align-items-center justify-content-center">
-                    <h2>Voter's Certificate 15 Request</h2>
-                </div>
-                <div class="col col-md-4 bg-success text-white p-5 shadow rounded d-flex align-items-center justify-content-center">
-                    <h2>PSA 9 Request</h2>
-                </div>
-            </div>
-            <div class="row justify-content-center gap-5 mt-5">
-                <div class="col col-md-4 bg-success text-white p-5 shadow rounded d-flex align-items-center justify-content-center">
-                    <h2>Kasalang Bayan 6 Request</h2>
-                </div>
-                <div class="col col-md-4 bg-success text-white p-5 shadow rounded d-flex align-items-center justify-content-center">
-                    <h2>Burial Assistance 10 Request</h2>
-                </div>
-            </div>
+        <div class="row justify-content-center mt-4 gap-3">
+            <a href="#" style="box-shadow: 1px 0px 5px 9px rgba(255,255,255,0.6) inset;"
+                class="col border-0 btn col-md-3 p-4 text-center text-white bg-success">
+                <img src="../src/assets/wedding.png"
+                    class="rounded-circle border" style="height: 150px; width: 150px;" alt="voters certificate"><br><br>
+                <h4>kasalang Bayan</h4>
+            </a>
+            <a href="#" style="box-shadow: 1px 0px 5px 9px rgba(255,255,255,0.6) inset;"
+                class="col border-0 btn col-md-3 p-4 text-center text-white bg-success ">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTW_J19b71VOHsJ626yP3ldsurEJWwUX7BwZw&usqp=CAU"
+                    class="rounded-circle border" style="height: 150px; width: 150px;" alt="voters certificate"><br><br>
+                <h4>Burrial Assistance</h4>
+            </a>
         </div>
     </div>
     <script>
